2f6d97b6abf8be87deedce110dd6c88b2c3af5db
Jayleaf1125 <Jasonpadilla104@gmail.com>
2021-10-13 14:26:10 +0000

    Refactored function 3

--- index.js
+++ index.js
@@ -27,19 +27,15 @@ function oddAndEvenInFifteen() {
 
 //3
 function assignGrade(int) {
-    if (int === 91 || int === 92 || int === 93 || int === 94 || int === 95 || int === 96 || int === 97 || int === 98 || int === 99 || int === 100) {
+    if (100 === int || int >= 90) {
         return "A";
-    }
-    else if (int === 81 || int === 82 || int === 83 || int === 84 || int === 85 || int === 86 || int === 87 || int === 88 || int === 89 || int === 90) {
+    } else if (90 === int || int >= 81) {
         return "B";
-    }
-    else if (int === 71 || int === 72 || int === 73 || int === 74 || int === 75 || int === 76 || int === 77 || int === 78 || int === 79 || int === 80) {
-        return "C";
-    }
-    else if (int === 65 || int === 66 || int === 67 || int === 68 || int === 69 || int === 70) {
+    } else if (80 === int || int >= 71) {
+        return "C"
+    } else if (70 === int || int >= 65) {
         return "D";
-    }
-    else if (int < 65) {
+    } else if (int < 65) {
         return "F";
     }
     else {
@@ -47,7 +43,25 @@ function assignGrade(int) {
     }
 }
 
+// function assignGradeRefactor(int) {
+//     if (100 === int || int >= 90) {
+//         return "A";
+//     } else if (90 === int || int >= 81) {
+//         return "B";
+//     } else if (80 === int || int >= 71) {
+//         return "C"
+//     } else if (70 === int || int >= 65) {
+//         return "D";
+//     } else if (int < 65) {
+//         return "F";
+//     }
+//     else {
+//         return null;
+//     }
+// }
+
 // Call.
 console.log(sumOfFourAndSix());
 oddAndEvenInFifteen();
-console.log(assignGrade(55));
+console.log(assignGrade(95));
+
